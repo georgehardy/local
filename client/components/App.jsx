70caa05953ff6186fd0a7c9182c1a99446fa485e
georgehardy <george.hardy@gmail.com>
2018-09-04 20:21:20 +1200

    initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+const App = props => {
+  return (
+    <div>
+      <h1>LOCAL</h1>
+    </div>
+  )
+}
+
+export default App
